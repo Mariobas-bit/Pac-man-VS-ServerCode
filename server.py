f9f4d93f496fdaca92730a584ed684c57723c67d
Mariobas-bit <Mariobas1325@gmail.com>
2026-09-06 19:53:05 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
+import eventlet
+eventlet.monkey_patch()
+
 import os
 from flask import Flask
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 
 @app.route('/')
 def home():
